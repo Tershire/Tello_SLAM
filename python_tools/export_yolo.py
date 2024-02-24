24ba124d054571f32604d41bd2a927795a173695
tershire <tershire@gmail.com>
2024-02-24 13:08:51 +0100

    made code cleaner.

--- python_tools/export_yolo.py
+++ python_tools/export_yolo.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-# Load a YOLOv8 model
-# model = YOLO("./runs/detect/train5/weights/best.pt")
-model = YOLO("./models/yolov8n.pt")
+# load
+model = YOLO("./runs/detect/train5/weights/best.pt")
+# model = YOLO("./models/yolov8n.pt")
 
-# Export the model
-# model.export(format="onnx", opset=12, simplify=True, dynamic=False, imgsz=640)
-model.export(format="onnx", opset=12, imgsz=640)
+# export
+model.export(format="onnx", opset=12, simplify=True, dynamic=False, imgsz=640)
+# model.export(format="onnx", opset=12, imgsz=640)
