693c5c78f5c6c8a2a79d25abc1c936ddb5405794
tershire <tershire@gmail.com>
2024-03-07 19:56:19 +0100

    i don't remember.

--- test/tello_vision_test_minimal.cpp
+++ test/tello_vision_test_minimal.cpp
@@ -1,20 +1,20 @@
-// tello_vision_test.cpp
+// tello_vision_test_minimal.cpp
 
 /////////////////////////
 // IONLAB ISAE-SUPAERO //
 // TELLO SLAM PROJECT  //
 /////////////////////////
 
-// 2024 JAN 15
+// 2024 MAR 07
 // Wonhee LEE
 
-// reference: https://github.com/HerrNamenlos123/tello/tree/master
+// reference:
 
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-#include "tello.hpp"
+// #include "tello.hpp"
 
 // using namespace tello_slam;
 
@@ -24,13 +24,13 @@ int main(int argc, char **argv)
     cv::VideoCapture cap{"udp://0.0.0.0:11111", cv::CAP_FFMPEG};
     cv::Mat frame;
 
-    Tello tello;
-    if (!tello.connect()) 
-    {
-        return -1;
-    }
+    // Tello tello;
+    // if (!tello.connect()) 
+    // {
+    //     return -1;
+    // }
 
-    tello.enable_video_stream();
+    // tello.enable_video_stream();
     //
     // tello.takeoff();
 
